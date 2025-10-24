b437034a97593dbc48ec0bc2d805f77b35349734
Alif Rizqullah M <125651171+alifrizqullahmaruf@users.noreply.github.com>
2025-10-24 13:37:21 +0700

    changes 1

--- app/_common/component/molecules/StockItem.tsx
+++ app/_common/component/molecules/StockItem.tsx
@@ -30,7 +30,7 @@ export default function StockItem({
             "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
           }
           src={logo}
-          // alt={`${name}_logo`}
+          alt={`${name}_logo`}
           width={76}
           height={76}
         >
